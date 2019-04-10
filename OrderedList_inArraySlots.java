204ed048f0a7f57264d5149b612c9045f7b295cd
lrhasher <lrhasher@gmail.com>
2019-04-10 13:19:19 -0400

    v0

---
+++ OrderedList_inArraySlots.java
@@ -0,0 +1,79 @@
+/**
+  OrderedList_inArraySlots, holding Integers
+
+  A list with the invariant the elements are in order by
+  increasing value, implemented using a
+  ArrayList<Integer>
+ */
+
+public class OrderedList_inArraySlots
+    implements OrderedList {
+
+    private java.util.ArrayList<Integer> list_iAS;
+
+    public OrderedList_inArraySlots() {
+        list_iAS = new java.util.ArrayList<Integer>();
+    }
+
+    /**
+      @return the number of elements in this list
+     */
+    public int size(){
+        return list_iAS.size();
+    }
+
+    /**
+      @return a string representation of this Orderedlist_iAS
+     */
+    public String toString() {
+        return list_iAS.toString();
+    }
+
+
+    /**
+      Put @value where it belongs in the list.
+      @pre: the list is in increasing order
+     */
+     public boolean add( Integer value) {
+         int dest = 0;
+         for( ; dest < list_iAS.size() && list_iAS.get( dest) < value
+              ; dest++) ;
+         // System.out.println( "OL adding " + value
+         //                   + " at index " + dest);
+         list_iAS.add( dest, value);
+         return true;
+     }
+
+
+     /**
+      @return element @index from this list
+      precondition: @index is within the bounds of the array.
+          (Having warned the user about this precondition,
+           you should NOT complicate your code to check
+           whether the condition was violated.)
+     */
+    public Integer get( int index ) {
+        return list_iAS.get( index);
+    }
+
+
+    /**
+      Remove the element at position @index in this list.
+
+      Shift any subsequent elements to the left (that is,
+      decrease the index associated with each).
+
+      @return the value that was removed from the list
+     */
+    public Integer remove( int index) {
+        return list_iAS.remove( index);
+    }
+
+    public int indexOf( Integer value) {
+        int index , compareVal;
+        for( index = size() / 2;
+             (compareVal = get(index).compareTo(value)) != 0;
+             index -= Math.signum(compareVal));
+        return index;
+    }
+}
